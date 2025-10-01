c2a5f663e599211ddb2ec519338f8af623b55ffd
apsschotten <ana.schotten@estudante.ifms.edu.br>
2025-10-01 13:04:17 -0400

    Update "script.js".
    > Create function "subtrair".

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function somar() {
     let resultadoSoma = valorX + valorY;
     document.getElementById('resultado_soma').value = resultadoSoma;
 };
+
+function subtrair() {
+    let valorX = document.getElementById('valorX_sub').value;
+    let valorY = document.getElementById('valorY_sub').value;
+    let resultadoSubtração = valorX - valorY;
+    document.getElementById('resultado_sub').value = resultadoSubtração;
+};
