7a07d2f74cd35e9f1a096cdcdbaf6cb3b26130e8
Ana Paula S. Schotten <ana.schotten@estudante.ifms.edu.br>
2025-10-01 00:07:20 -0400

    Update "script.js".
    > Create function "dividir".

---
+++ script.js
@@ -0,0 +1,6 @@
+function dividir() {
+    let valorX = document.getElementById('valorX_div').value;
+    let valorY = document.getElementById('valorY_div').value;
+    let resultadoDivisão = valorX / valorY;
+    document.getElementById('resultado_div').value = resultadoDivisão;
+};
